6ffb35e80a728fdf8261988308e56b8fdb4b223e
Argestis <alexandronav@gmail.com>
2017-01-08 22:40:51 +0000

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,61 @@
+class PostsController < ApplicationController
+    before_action :find_post, only: [:show, :edit, :update, :destroy]
+    
+    def index
+        @post = Post.all.order("created_at DESC")
+        
+        
+    end
+    
+    def new
+      
+      @post  = Post.new   
+      
+    end
+    
+    def create
+    
+      @post = Post.new post_params
+      
+      if @post.save
+         redirect_to @post, notice: "Article successfully saved"
+      else
+         render 'new', notice: "Article was not saved" 
+      end
+    
+    end
+    
+    def show
+    end
+    
+    def edit 
+    end
+    
+    def destroy
+      @post.destroy
+      redirect_to posts_path
+    end
+    
+    def update 
+        if @post.update post_params
+            redirect_to @post, notice: "Article successfully updated"
+        else
+            render 'edit'
+        end
+            
+    end
+    
+    private
+    
+    def post_params
+      
+      params.require(:post).permit(:title, :content) 
+        
+    end
+    
+    def find_post
+       @post = Post.find(params[:id])
+    end
+ 
+    
+end
